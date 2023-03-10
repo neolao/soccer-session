ce7c236000430a921a0d3be2d75d96f95fb14076
neolao <contact@neolao.com>
2023-03-10 21:51:18 +0000

    up

--- www/index.php
+++ www/index.php
@@ -36,6 +36,7 @@ function saveSession($filePath, $session) {
     <head>
         <title>Soccer session</title>
         <link rel="stylesheet" type="text/css" href="style.css"/>
+        <link rel="icon" href="/favicon.png"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
     <body>
